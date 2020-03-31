e4a8878189d826c5c81b9a011fb31e412b207352
ReidHolben <35782114+ReidHolben@users.noreply.github.com>
2020-03-31 13:57:03 -0600

    Add files via upload

---
+++ Shiny App.R
@@ -0,0 +1,90 @@
+library(mapsapi)
+library(leaflet)
+library(shiny)
+
+data <- read.csv('~/Documents/simulatedData.csv')
+data <- data[1:10,]
+
+# Vectors for origins and destinations.
+origin_vector <- paste0(data$HSE_NBR_home,' ', data$STREET_home, ' ', data$STTYPE_home, ', Milwaukee, WI ', data$ZIP_CODE_home)
+destination_vector <- paste0(data$HSE_NBR,' ', data$STREET, ' ', data$STTYPE, ', Milwaukee, WI ', data$ZIP_CODE)
+
+doc=mp_directions(
+  origin = origin_vector[1],
+  destination = destination_vector[1],
+  alternatives=FALSE,
+  key='AIzaSyAwCU0w7-3pLYwtSW_6tA0yRi7B5ENYsGg'
+)
+
+routes <- mp_get_routes(doc)
+
+for(x in 2:length(origin_vector)){
+  doc <- mp_directions(
+    origin = origin_vector[x],
+    destination = destination_vector[x],
+    alternatives = FALSE,
+    key='AIzaSyAwCU0w7-3pLYwtSW_6tA0yRi7B5ENYsGg'
+  )
+  route <- mp_get_routes(doc)
+  
+  routes <- rbind(route, routes)
+}
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
+
+ui <- fluidPage(
+  leafletOutput("mymap"),
+  p(),
+    titlePanel("Actions"),
+    
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar to demonstrate various slider options ----
+    sidebarPanel(
+      
+      # Input: Simple integer interval ----
+      sliderInput("integer", "Time Factor:",
+                  min = 1, max = 3,
+                  value = 2),
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Table summarizing the values entered ----
+      tableOutput("values")
+      
+    )
+  )
+)
+pal <- colorFactor(palette='Dark2', domain=rownames(routes))
+server <- function(input, output, session) {
+  
+  points <- eventReactive(input$recalc, {
+    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+  }, ignoreNULL = FALSE)
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      addPolylines(data = routes[1], opacity = 1, weight = 7, color = ~pal(alternative_id))
+  })
+  
+  sliderValues <- reactive({
+    
+    data.frame(
+      Name = c("Time Factor"),
+      Value = as.character(c(input$integer)),
+      stringsAsFactors = FALSE)
+    
+  })
+  
+  # Show the values in an HTML table ----
+  output$values <- renderTable({
+    sliderValues()
+  })
+}
+
+shinyApp(ui, server)
